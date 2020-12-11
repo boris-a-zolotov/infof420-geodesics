29725870de2a0e427b1e06ed3b1b57806133c9ac
boris.a.zolotov @ hp15 <boris.a.zolotov@yandex.com>
2020-12-12 01:04:12 +0300

    edges removed

--- js-boris/index.js
+++ js-boris/index.js
@@ -13,28 +13,6 @@ let globalArray = [1, 0, 3, 1, 2, 3, 0, 2,
 let faces = []
 
 
-// DESCRIPTION OF EDGES
-// face, edge no., face, edge no.
-// need to generate
-
-let edges = [
-    [0, 0, 1, 2],
-    [1, 0, 2, 2],
-    [2, 0, 3, 2],
-    [3, 0, 0, 2],
-
-    [4, 3, 1, 1],
-    [1, 3, 5, 1],
-    [5, 3, 3, 3],
-    [4, 1, 3, 1],
-
-    [0, 3, 5, 2],
-    [0, 1, 4, 2],
-    [4, 0, 2, 1],
-    [5, 0, 2, 3]
-]
-
-
 // Return direction of the i-th edge of the face
 
 // i-th edge is between vertices i and i+1
