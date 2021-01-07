976261da00493b91e5aed3bf971758fb7e7d3062
boris.a.zolotov @ hp15 <boris.a.zolotov@yandex.com>
2021-01-07 19:22:41 +0300

    copy-paste functions

--- js-boris/index-pyramid-sort.js
+++ js-boris/index-pyramid-sort.js
@@ -62,6 +62,46 @@ function arraysEq(arr1, arr2) {
     return true;
 };
 
+// removing duplicates from a multidimensional array
+
+function multiDimensionalUnique(arr) {
+    var uniques = [];
+    var itemsFound = {};
+    for(var i = 0, l = arr.length; i < l; i++) {
+        var stringified = JSON.stringify(arr[i]);
+        if(itemsFound[stringified]) { continue; }
+        uniques.push(arr[i]);
+        itemsFound[stringified] = true;
+    }
+    return uniques;
+}
+
+// for each vertex make the face with
+// the smallest index the first face to appear
+
+function rotateMin(arr) {
+    let locLen = arr.length;
+    let locMin = 1000;
+    let minPos = 1000;
+    let resArr = [];
+
+    for (var i = 0; i < locLen; i += 2) {
+        // we assume we deal with planar faces,
+        // which means two vertices of one face
+        // can't glue to each other
+        if (arr[i] < locMin) {
+            locMin = arr[i];
+            minPos = i;
+        }
+    }
+
+    for (var i = 0; i < locLen; i++) {
+        resArr = resArr.concat(arr[(i + minPos) % locLen]);
+    }
+
+    return resArr;
+};
+
 
 // PREPARING VERTICES TO CHECK EACH OF THEM IS CONVEX
 
