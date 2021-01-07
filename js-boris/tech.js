34f5b016d74242320e0e3797e8bd6d488d46b890
boris.a.zolotov @ hp15 <boris.a.zolotov@yandex.com>
2021-01-07 17:55:55 +0300

    isLonely is not what i need

--- js-boris/tech.js
+++ js-boris/tech.js
@@ -1,3 +1,11 @@
+function arraysEq(arr1, arr2) {
+    if (arr1.length !== arr2.length) return false;
+    for (var i = 0; i < arr1.length; i++) {
+        if (arr1[i] !== arr2[i]) return false;
+    }
+    return true;
+};
+
 // for each vertex make the face with
 // the smallest index the first face to appear
 
@@ -23,3 +31,80 @@ function rotateMin(arr) {
 
     return resArr;
 };
+
+function isLonely(x, arr) {
+    let locSum = 0;
+
+    for (var i = 0; i < arr.length; i++) {
+        if (arraysEq (x, arr[i])) locSum++;
+    }
+
+    if (locSum > 1) return false;
+    return true;
+}
+
+
+let vertices = [
+    [
+        0, 0, 4, 2,
+        5, 2, 1, 0
+    ],
+    [0, 1, 3, 1, 4, 1],
+    [
+        0, 2, 1, 1,
+        2, 1, 3, 0
+    ],
+    [
+        1, 0, 0, 0,
+        4, 2, 5, 2
+    ],
+    [
+        1, 1, 2, 1,
+        3, 0, 0, 2
+    ],
+    [1, 2, 5, 0, 2, 0],
+    [2, 0, 1, 2, 5, 0],
+    [
+        2, 1, 3, 0,
+        0, 2, 1, 1
+    ],
+    [
+        2, 2, 5, 1,
+        4, 0, 3, 2
+    ],
+    [
+        3, 0, 0, 2,
+        1, 1, 2, 1
+    ],
+    [3, 1, 4, 1, 0, 1],
+    [
+        3, 2, 2, 2,
+        5, 1, 4, 0
+    ],
+    [
+        4, 0, 3, 2,
+        2, 2, 5, 1
+    ],
+    [4, 1, 0, 1, 3, 1],
+    [
+        4, 2, 5, 2,
+        1, 0, 0, 0
+    ],
+    [5, 0, 2, 0, 1, 2],
+    [
+        5, 1, 4, 0,
+        3, 2, 2, 2
+    ],
+    [
+        5, 2, 1, 0,
+        0, 0, 4, 2
+    ]
+];
+
+for (var i = 0; i < vertices.length; i++) {
+    vertices[i] = rotateMin(vertices[i]);
+}
+
+vertices = vertices.filter(x => isLonely(x, vertices));
+
+console.log(vertices);
